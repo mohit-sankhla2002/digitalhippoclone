a9f580583db0bd2dd0c407797fe07cbe1b0381d8
mohit-sankhla2002 <100336545+mohit-sankhla2002@users.noreply.github.com>
2023-11-29 17:51:37 +0530

    trpc setup

---
+++ src/trpc/index.ts
@@ -0,0 +1,9 @@
+import { router, publicProcedure } from "./trpc";
+
+export const appRouter = router({
+    anyApiRoute: publicProcedure.query(() => {
+        return 'hello';
+    })
+});
+
+export type AppRouter = typeof appRouter;
