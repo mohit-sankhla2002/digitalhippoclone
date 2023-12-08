b9ddd25e6b8e99a23f5c11dbfe2d2a524484fe5c
mohit-sankhla2002 <100336545+mohit-sankhla2002@users.noreply.github.com>
2023-12-08 12:48:50 +0530

    fixed typo approvedForSale to approved_for_sale

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -5,45 +5,65 @@ import { z } from 'zod';
 import { getPayloadClient } from '../get-payload'
 export const appRouter = router({
     auth: authRouter, 
-    getInfiniteProducts: publicProcedure.input(z.object({
-        limit: z.number().min(1).max(100), 
+    getInfiniteProducts: publicProcedure
+    .input(
+      z.object({
+        limit: z.number().min(1).max(100),
         cursor: z.number().nullish(),
-        query: QueryValidator, 
-    })).query(async ({ input, ctx }) => {
-        const { query, cursor } = input;
-        const { sort, limit, ...queryOpts } = query;
-        
-        const payload = await getPayloadClient();
+        query: QueryValidator,
+      })
+    )
+    .query(async ({ input }) => {
+      const { query, cursor } = input
+      const { sort, limit, ...queryOpts } = query
 
-        const parsedQueryOpts: Record<string, {equals: string}> = {};
+      const payload = await getPayloadClient();
 
-        Object.entries(queryOpts).forEach(([key, value]) => {
-            parsedQueryOpts[key] = {
-                equals: value
-            }
-        });
+      const parsedQueryOpts: Record<
+        string,
+        { equals: string }
+      > = {}
 
-        const page = cursor || 1;
-
-        const { docs: items, hasNextPage, nextPage } = await payload.find({
-            collection: "products", 
-            where: {
-                approvedForSale: {
-                    equals: "approved"
-                }, 
-                ...parsedQueryOpts
-            },
-            sort, 
-            depth: 1, 
-            limit, 
-            page,
-        });
-
-        return {
-            items, 
-            nextPage: hasNextPage ? nextPage : null
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
+          equals: value,
         }
-    })
+      })
+
+      const page = cursor || 1;
+
+      try {
+        const {
+            docs: items,
+            hasNextPage,
+            nextPage,
+          } = await payload.find({
+            collection: 'products',
+            where: {
+              approved_for_sale: {
+                equals: 'approved',
+              },
+              ...parsedQueryOpts,
+            },
+            sort,
+            depth: 1,
+            limit,
+            page,
+          })
+    
+          console.log("items", items);
+    
+          return {
+            items,
+            nextPage: hasNextPage ? nextPage : null,
+          }
+      } catch (error) {
+        console.log(error);
+        throw new Error("Something Went Wrong")
+      }
+
+      
+    }),
 });
 
 export type AppRouter = typeof appRouter;
